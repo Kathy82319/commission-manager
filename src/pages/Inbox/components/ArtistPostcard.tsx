b96a92c476d032f0cd409626fec2d392b3165390
Kathy82319 <cath82319@gmail.com>
2026-05-01 15:40:54 +0800

    0.0

--- src/pages/Inbox/components/ArtistPostcard.tsx
+++ src/pages/Inbox/components/ArtistPostcard.tsx
@@ -144,8 +144,7 @@ export const ArtistPostcard: React.FC<ArtistPostcardProps> = ({ item, snapshot, 
             </div>
           )}
 
-          {/* 🌟 針對按鈕區塊進行修改：加入 flex 排版與縮小按鈕的設定 */}
-          <div className="postcard-actions-wrapper mt-4 flex justify-end items-center gap-3 [&>*]:!text-sm [&>*]:!px-4 [&>*]:!py-1.5">
+          <div className="postcard-actions-wrapper mt-4">
             {children}
           </div>
         </div>
