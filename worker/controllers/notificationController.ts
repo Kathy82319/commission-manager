3790781e7ebfd0dfd4499062a7be209e4f818720
Kathy82319 <cath82319@gmail.com>
2026-05-19 22:20:48 +0800

    設置好郵件的變數

--- worker/controllers/notificationController.ts
+++ worker/controllers/notificationController.ts
@@ -210,5 +210,5 @@ export const notificationController = {
     } catch (error: any) {
       return new Response(JSON.stringify({ success: false, error: error.message }), { status: 500, headers: corsHeaders });
     }
-  }
-};
+  } 
+}; 
