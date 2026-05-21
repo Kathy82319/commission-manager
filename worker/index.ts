dd66c227df8f2e00f283c4d7bf6e996641ca4b0b
Kathy82319 <cath82319@gmail.com>
2026-05-21 16:44:17 +0800

    0.0

--- worker/index.ts
+++ worker/index.ts
@@ -309,8 +309,8 @@ export default {
       }
 
       if (request.method === "GET" && sanitizedPath === "/api/auth/line/login") return authController.login(request, env, corsHeaders);
-      if (request.method === "GET" && sanitizedPath === "/api/auth/line/callback  ") return authController.callback(request, env, corsHeaders);
-      
+      if (request.method === "GET" && sanitizedPath === "/api/auth/line/callback") return authController.callback(request, env, corsHeaders);
+
       if (request.method === "POST" && sanitizedPath === "/api/auth/logout") {
         return authController.logout(request, env, corsHeaders);
       }
