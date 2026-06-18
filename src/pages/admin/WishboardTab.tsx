e2f5dd9890fbca559f87c4275cfc61a0736be00f
Kathy82319 <cath82319@gmail.com>
2026-06-18 21:07:16 +0800

    0.0

--- src/pages/admin/WishboardTab.tsx
+++ src/pages/admin/WishboardTab.tsx
@@ -64,7 +64,7 @@ export function WishboardTab() {
       setIsUpdating(false);
     }
   };
-
+ 
   const openReportModal = async (bulletinId: string) => {
     setSelectedBulletinId(bulletinId);
     setIsModalOpen(true);
