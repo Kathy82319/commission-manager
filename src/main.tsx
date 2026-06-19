b4814ace1ca09b3daf6d1069e2a306159e923d6c
Kathy82319 <cath82319@gmail.com>
2026-06-19 18:37:04 +0800

    0.0

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,8 @@
-/// <reference types="vite-plugin-pwa/client" />
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import App from './App.tsx'
-
-import { registerSW } from 'virtual:pwa-register'
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  e.preventDefault();
-});
-
-if ('serviceWorker' in navigator) {
-  if ('caches' in window) {
-    caches.delete('arti-pages-cache').catch(() => {});
-  }
-
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    for (const registration of registrations) {
-      const swOrigin = registration.active?.scriptURL
-        ? new URL(registration.active.scriptURL).origin
-        : null;
-      if (swOrigin && swOrigin !== window.location.origin) {
-        registration.unregister();
-      }
-    }
-  }).catch(() => {});
-
-  registerSW({ immediate: true });
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
