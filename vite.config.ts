ba06e9954f7e97ddd53bd35b8fb2fd31e7d93e21
Kathy82319 <cath82319@gmail.com>
2026-06-19 18:22:58 +0800

    清除安裝插件的問題

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,10 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      injectRegister: 'auto',
-      strategies: 'injectManifest',
-      srcDir: 'src',
-      filename: 'sw.ts',
-      includeAssets: ['vite.svg', 'pwa-192x192.png', 'pwa-512x512.png'],
-
-      manifest: {
-        name: 'Arti 繪師小幫手',
-        short_name: 'Arti',
-        theme_color: '#5D4A3E',
-        background_color: '#FFFFFF',
-        display: 'standalone',
-        orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
-        icons: [
-          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' }
-        ]
-      },
-
-      injectManifest: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
-      },
-    })
   ],
   server: {
     proxy: {
